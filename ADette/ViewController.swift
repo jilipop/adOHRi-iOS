6e7ef23ae14692446c9c7ff3cd7a98b71c4cca23
Hubert Popiolek <jilipop@web.de>
2021-12-30 12:41:57 +0100

    Added start/stop button functionality and setting the audio session category

--- ADette/ViewController.swift
+++ ADette/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import AVFAudio
 
 class ViewController: UIViewController {
 
+    let audioSession = AVAudioSession.sharedInstance()
+    let player = Player()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        do {
+            if #available(iOS 11.0, *) {
+                try audioSession.setCategory(.playback, mode: .spokenAudio, policy: .longForm)
+                print("audio session category set successfully")
+            } else {
+                try audioSession.setCategory(.playback, mode: .spokenAudio)
+                print("audio session category set successfully")
+            }
+        } catch {
+            print("Failed to set audio session category. Error: \(error)")
+        }
     }
-
-
+    
+    @IBAction func playStopAction(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        if sender.isSelected {
+            player.start()
+            sender.setTitle("Audiodeskription stoppen", for: UIControl.State.normal)
+        } else {
+            player.stop()
+            sender.setTitle("Audiodeskription starten", for: UIControl.State.normal)
+      }
+    }
 }
 
