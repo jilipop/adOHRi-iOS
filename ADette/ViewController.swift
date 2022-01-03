463bc6ba9c67d2d801b1b5ef4f7186e78586c6ec
Hubert Popiolek <jilipop@web.de>
2022-01-03 18:02:43 +0100

    Moved setting the audio session category to Player.swift

--- ADette/ViewController.swift
+++ ADette/ViewController.swift
@@ -9,24 +9,12 @@ import UIKit
 import AVFAudio
 
 class ViewController: UIViewController {
-
-    let audioSession = AVAudioSession.sharedInstance()
+    
     let player = Player()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        do {
-            if #available(iOS 11.0, *) {
-                try audioSession.setCategory(.playback, mode: .spokenAudio, policy: .longForm)
-                print("audio session category set successfully")
-            } else {
-                try audioSession.setCategory(.playback, mode: .spokenAudio)
-                print("audio session category set successfully")
-            }
-        } catch {
-            print("Failed to set audio session category. Error: \(error)")
-        }
     }
     
     @IBAction func playStopAction(_ sender: UIButton) {
