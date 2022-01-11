13450108d9399a964f35cca4f26c91ec06dfc2ea
Hubert Popiolek <jilipop@web.de>
2022-01-11 14:40:55 +0100

    ramp up the jitter, remove unnecessary constants

--- ADette/constants.h
+++ ADette/constants.h
@@ -3,12 +3,11 @@
 
 #define ADDR "::"
 #define PORT 1350
-#define JITTER 16
+#define JITTER 60
 #define RATE 48000
 #define CHANNELS 2
 #define PAYLOAD_0_REFERENCE_RATE 8000
 #define BUFFER_LENGTH 122880
-#define PACKET_DURATION 40 /* in ms, defined by sender */
-#define SAMPLES 1920 /* (PACKET_DURATION * RATE / 1000) */
+#define FRAME_SIZE 960 /* defined by sender */
 
 #endif
