74d61f5f4ea3ed78f3e746129037efdc12beb1ef
Hubert Popiolek <jilipop@web.de>
2022-01-14 00:20:17 +0100

    Add wifi connection logic

--- ADette/ViewController.swift
+++ ADette/ViewController.swift
@@ -11,6 +11,7 @@ import AVFAudio
 class ViewController: UIViewController {
     
     let player = Player()
+    let wifi = WiFiManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,10 +21,14 @@ class ViewController: UIViewController {
     @IBAction func playStopAction(_ sender: UIButton) {
         sender.isSelected.toggle()
         if sender.isSelected {
+            if !wifi.isADConnected() {
+                wifi.connect()
+            }
             player.start()
             sender.setTitle("Audiodeskription stoppen", for: UIControl.State.normal)
         } else {
             player.stop()
+            wifi.remove()
             sender.setTitle("Audiodeskription starten", for: UIControl.State.normal)
       }
     }
---
+++ ADette/WiFiManager.swift
@@ -0,0 +1,36 @@
+import Foundation
+import NetworkExtension
+import SystemConfiguration.CaptiveNetwork
+
+class WiFiManager {
+    let wiFiCredentials = NEHotspotConfiguration(ssid: "Audio-Deskription", passphrase: "klappeauf!", isWEP: false)
+    let config = NEHotspotConfigurationManager()
+    
+    func isADConnected() -> Bool {
+        var ssid: String?
+        if let interfaces = CNCopySupportedInterfaces() as NSArray? {
+            for interface in interfaces {
+                if let interfaceInfo = CNCopyCurrentNetworkInfo(interface as! CFString) as NSDictionary? {
+                    ssid = interfaceInfo[kCNNetworkInfoKeySSID as String] as? String
+                    break
+                }
+            }
+        }
+        return ssid == wiFiCredentials.ssid ? true : false
+    }
+    
+    func connect() {
+        config.apply(wiFiCredentials) { error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            else {
+                print("connecting to AD wifi")
+            }
+        }
+    }
+    
+    func remove() {
+        config.removeConfiguration(forSSID: wiFiCredentials.ssid)
+    }
+}
