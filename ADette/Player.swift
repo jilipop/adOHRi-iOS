be1a307ca79c079061c0fd681cb8135361ad28cf
Hubert Popiolek <jilipop@web.de>
2022-02-08 10:20:10 +0100

    Update debug comment

--- ADette/Player.swift
+++ ADette/Player.swift
@@ -114,7 +114,7 @@ class Player {
         } catch {
             print("Failed to stop audio session. Error: \(error)")
         }
-        //TODO: Bug – possible negative buffer fillcount on cleanup, due to "tail" value being larger than "head" value (when only silence was played?)
+        //TODO: Potential bug – possible negative buffer fillcount (on cleanup?), due to "tail" value being larger than "head" value (when mostly silence was played?) - can't reproduce lately
         TPCircularBufferCleanup(&circularBuffer)
     }
 }
